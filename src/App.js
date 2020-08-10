1cfef30f1ca08d93bf9b9817c1f5514e134fd35d
Demetrio <demetrioalmeida@gmail.com>
2020-08-10 00:25:36 -0700

    working application

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React, { Component } from "react";
 import "./App.css";
-import getUsers from "./utils/API.js";
-import Main from "./components/Main";
-import Header from "./components/Header";
+import Search from "./components/Search";
 
 class App extends Component {
   render() {
-    console.log(getUsers);
-    return (
-      <div>
-        <Header />
-        <Main />
-      </div>
-    );
+    return <Search />;
   }
 }
 
